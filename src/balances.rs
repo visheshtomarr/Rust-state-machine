d1969eda16b705149b447ab5befe1ed740c5d3d9
visheshtomarr <visheshtomar7800@gmail.com>
2024-07-17 20:47:03 +0530

    Implemented set_balance and balance function

--- src/balances.rs
+++ src/balances.rs
@@ -13,4 +13,15 @@ impl Pallet {
             balances: BTreeMap::new(),
         }
     }
+
+    /// Set the balance of an account "who" to some "amount".
+    pub fn set_balance(&mut self, who: &String, amount: u128) {
+        self.balances.insert(who.clone(), amount) ;
+    }
+
+    /// Get the balance of an account "who".
+    /// If the account has no stored balance, we return zero.
+    pub fn balance(&self, who: &String) -> u128 {
+        *self.balances.get(who).unwrap_or(&0) 
+    }
 }
