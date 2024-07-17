af67d27648145fdc9f62890a73b04dde7fe95f4d
visheshtomarr <visheshtomar7800@gmail.com>
2024-07-17 20:37:22 +0530

    Added Pallet struct for balances module

--- src/balances.rs
+++ src/balances.rs
@@ -1,1 +1,16 @@
-pub struct Pallet {}
+use std::collections::BTreeMap ; 
+
+/// This is the Balances module.
+/// It is a simple module that keeps track of how much balance a user has in our state machine.
+pub struct Pallet {
+    balances: BTreeMap<String, u128>,
+}
+
+impl Pallet {
+    /// Create a new instance of our balances module.
+    pub fn new(&self) -> Self {
+        Self {
+            balances: BTreeMap::new(),
+        }
+    }
+}
