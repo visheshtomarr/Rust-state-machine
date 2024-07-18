eda0e1fe5a82afe4d09c54552bbac8493271f9e4
visheshtomarr <visheshtomar7800@gmail.com>
2024-07-18 19:26:20 +0530

    Implemented transfer function and added test

--- src/balances.rs
+++ src/balances.rs
@@ -24,6 +24,31 @@ impl Pallet {
     pub fn balance(&self, who: &String) -> u128 {
         *self.balances.get(who).unwrap_or(&0) 
     }
+
+    /// Transfer some "amount" from one account to another.
+    /// This function verifies that "from" has atleast "amount" balance to transfer and that no
+    /// mathematical overflow occurs.
+    pub fn transfer(
+        &mut self, 
+        caller: String,
+        to: String,
+        amount: u128
+    ) -> Result<(), &'static str> {
+
+        // Get balance of both user pre-transfer.
+        let caller_balance = self.balance(&caller) ;
+        let to_balance = self.balance(&to) ;
+
+        // Calculate new balances of both "caller" & "to" accounts while keeping check of underflow and overflow.
+        let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Insufficient funds.") ?;
+        let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow.") ?;
+
+        // Update balances of both accounts post-transfer.
+        self.balances.insert(caller, new_caller_balance) ;
+        self.balances.insert(to, new_to_balance) ;
+
+        Ok(()) 
+    }
 }
 
 #[cfg(test)]
@@ -41,5 +66,36 @@ mod tests {
         assert_eq!(balances.balance(&"alice".to_string()), 100) ;
         // Assert balance of "bob" has not changed and is equal to zero.
         assert_eq!(balances.balance(&"bob".to_string()), 0) ;
+    }
+
+    #[test]
+    fn transfer_balance() {
+        // Instantiating a balances struct
+        let mut balances = crate::balances::Pallet::new() ;
+        
+        // Alice cannot transfer funds she doesn't have.
+        assert_eq!(
+            balances.transfer("alice".to_string(), "bob".to_string(), 50),
+            Err("Insufficient funds.")
+        ) ;
+
+        // Providing alice with some balance.
+        balances.set_balance(&"alice".to_string(), 100) ;
+
+        // Alice can now transfer funds.
+        assert_eq!(
+            balances.transfer("alice".to_string(), "bob".to_string(), 55),
+            Ok(())
+        ) ;
+
+        // Check both accounts' balances updated successfully.
+        assert_eq!(balances.balance(&"alice".to_string()), 45) ;
+        assert_eq!(balances.balance(&"bob".to_string()), 55) ;
+
+        // Alice can no longer transfer funds greater than amount of 45.
+        assert_eq!(
+            balances.transfer("alice".to_string(), "bob".to_string(), 50),
+            Err("Insufficient funds.")
+        ) ;
     }
 }
