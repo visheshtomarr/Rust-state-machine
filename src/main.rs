d0d2a80a512a71fda4b97c1c627127dbef00909f
visheshtomarr <visheshtomar7800@gmail.com>
2024-07-21 19:55:31 +0530

    Added Runtime

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 mod balances ;
 mod system ;
 
+/// This is our main Runtime.
+/// It accumulates all the different pallets we want to use.
+pub struct Runtime {
+	system: system::Pallet,
+	balances: balances::Pallet,
+}
+
+impl Runtime {
+	/// Create a new instance of our main Runtime, by creating a new instance of each pallet.
+	pub fn new() -> Self {
+		Self {
+			system: system::Pallet::new(),
+			balances: balances::Pallet::new(),
+		}
+	}
+}
+
 fn main() {
 	println!("Hello, world!");
 }
