2b674fceff614f3baec891757fcf2146a0573827
visheshtomarr <visheshtomar7800@gmail.com>
2024-07-23 18:03:26 +0530

    Made system pallet generic over AccountId, BlockNumber & Nonce

--- src/system.rs
+++ src/system.rs
@@ -1,43 +1,46 @@
+use num::traits::{Zero, One} ; 
 use std::collections::BTreeMap ;
-
-type AccountId = String ;
-type BlockNumber = u32 ;
-type Nonce = u32 ;
+use core::ops::AddAssign ;
 
 /// This is the system Pallet.
 /// It handles low level state needed for our blockchain.
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountId, BlockNumber, Nonce> {
     /// The current block number.
     block_number: BlockNumber,
     /// A map from an account to their "nonce".
-    nonce: BTreeMap<String, Nonce>,
+    nonce: BTreeMap<AccountId, Nonce>,
 }
 
-impl Pallet {
+impl<AccountId, BlockNumber, Nonce> Pallet<AccountId, BlockNumber, Nonce>
+where 
+    AccountId: Ord + Clone,
+    BlockNumber: Zero + One + Copy + AddAssign,
+    Nonce: Zero + One + Copy, 
+{
     /// Create a new instance of the System pallet.
     pub fn new() -> Self {
         Self {
-            block_number: 0,
+            block_number: BlockNumber::zero(),
             nonce: BTreeMap::new() ,
         }
     }
 
     /// Get the current block number.
-    pub fn block_number(&self) -> u32 {
+    pub fn block_number(&self) -> BlockNumber {
         self.block_number
     }
 
     /// Increment the block number by one.
     pub fn inc_block_number(&mut self) {
-        self.block_number += 1 ;
+        self.block_number += BlockNumber::one() ;
     }
 
     /// Increment the nonce of an account. This helps us keep track of how many transactions
     /// each account has made.
     pub fn inc_nonce(&mut self, who: &AccountId) {
-        let nonce = *self.nonce.get(who).unwrap_or(&0) ;
-        let new_nonce = nonce + 1  ;
+        let nonce = *self.nonce.get(who).unwrap_or(&Nonce::zero()) ;
+        let new_nonce = nonce + Nonce::one()  ;
         self.nonce.insert(who.clone(), new_nonce) ;
     }
 }
@@ -47,7 +50,7 @@ mod test {
     #[test] 
     fn init_system() {
         // Instantiating a system struct.
-        let mut system = crate::system::Pallet::new() ;
+        let mut system = crate::system::Pallet::<String, u32, u32>::new() ;
 
         // Increment the current block number.
         system.inc_block_number() ;
